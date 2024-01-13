66654982da701f07be2538b739daba7b481a7331
ChienGv <giapvanchien070801@gmail.com>
2024-01-13 09:35:29 +0700

    Bo token api upload

--- src/routes/upload_route.js
+++ src/routes/upload_route.js
@@ -1,13 +1,8 @@
 const uploadController = require("../controlers/upload_controller");
 const upload = require("../common/uploadSingle");
-const verifyToken = require("../middlewares/jwt_middle");
 
 module.exports = function (router) {
-  router.post(
-    "/api/upload/thumbnail",
-    verifyToken,
-    uploadController.uploadThumbnail
-  );
+  router.post("/api/upload/thumbnail", uploadController.uploadThumbnail);
 
-  router.post("/api/upload/slides", verifyToken, uploadController.uploadSlides);
+  router.post("/api/upload/slides", uploadController.uploadSlides);
 };
